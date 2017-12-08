d2c8ad298199a15aa0d819dda4f8ccdea68f769d
Ryan Higgins <c16381886@mydit.ie>
2017-12-08 15:04:16 +0000

    Ryan Working on Map

---
+++ Assets/MapMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour {
+
+	public List<Transform> points;
+	Vector3 playerPos;
+	float distance;
+
+	// Use this for initialization
+	void Start () {
+		playerPos = transform.position;
+		points [0] = transform;
+		points [1] = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = 
+		if(Input.GetKey(KeyCode.D) && points[0].position == playerPos) {
+			playerPos
+		}
+	}
+}
